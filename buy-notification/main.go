fb7195125a20de56dd5f016b2f37c3f4db482044
ikedaema2018 <ikedaema0817@gmail.com>
2020-08-22 19:07:39 +0900

    http通信をする

--- buy-notification/main.go
+++ buy-notification/main.go
@@ -3,10 +3,20 @@ package main
 
 import (
         "github.com/aws/aws-lambda-go/lambda"
-        "net/http"
+				"net/http"
+				"fmt"
+				"io/ioutil"
 )
 
 func hello() (string, error) {
+	url := "http://google.co.jp"
+	func (){
+		res, _ := http.Get(url)
+		defer res.Body.Close()
+
+		byteArray, _ := ioutil.ReadAll(res.Body)
+		fmt.Println(string(byteArray))
+	}()
 	return "Hello ƛ!", nil
 }
 
