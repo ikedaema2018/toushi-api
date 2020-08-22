3b8fe457857e9decc0ea9c6311e7e3474ee76f9a
ikedaema2018 <ikedaema0817@gmail.com>
2020-08-22 15:06:21 +0900

    lambdaにdeploy

---
+++ buy-notification/deploy.sh
@@ -0,0 +1,8 @@
+GOOS=linux GOARCH=amd64 go build -o main main.go #ビルド
+zip main.zip main #zipに固める
+
+#zipをlambdaにアップロード
+aws lambda update-function-code \
+--region ap-northeast-1 \
+--function-name buy-notification \
+--zip-file fileb://./main.zip --publish
